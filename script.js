48add66e57b3a4a878467913efe58d4efb50182e
dmitryure <dmitryure@gmail.com>
2019-07-31 22:47:06 +0300

    + lines (skoro)

--- script.js
+++ script.js
@@ -5,23 +5,33 @@ const settings = {
   dimensions: [ 2048, 2048 ]
 };
 
-let points = 10000
+let points = 1000
 
 let x = 0
 let y = 2048
+
+let lineArr = []
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
     for (let i = 0; i < points; i++) {
+      lineArr.push([x, y])
       context.beginPath()
-      context.arc(x / 2, y / 2 , 2, 1, Math.PI* 2)
-      x = x + random.range(-10, 10)
-      y = y + random.range(-10, 10)
+      context.arc(x / 2, y / 2 , 3.5, 0, Math.PI* 2)
+      x = x + Math.abs(random.range(-20, 20))
+      y = y + Math.abs(random.range(-20, 20))
       context.fillStyle = 'black'
       context.fill()
-      console.log(x)
+    }
+    for (let i = 0; i < lineArr.length; i++) {
+      console.log(lineArr)
+      console.log(lineArr[i + 1][0], lineArr[i + 1][1])
+      context.beginPath()
+      context.moveTo(lineArr[i][0] / 2, lineArr[i][1] / 2)
+      context.lineTo(lineArr[i + 1][0] / 2, lineArr[i + 1][1] / 2)
+      context.stroke()
     }
   };
 };
